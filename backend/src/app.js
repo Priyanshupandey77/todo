5d052b53d72f13cf0af7150e8722288a1dfad0df
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-04-11 03:00:52 +0530

    fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,19 +8,12 @@ import errorHandler from "./middleware/errorMiddleware.js";
 const app = express();
 
 // Middleware
-// ✅ CORS FIRST
 app.use(
   cors({
     origin: "https://todo-xi-drab.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-
-// ✅ Handle preflight requests
-app.options("*", cors());
-
 app.use(express.json());
 
 // Test route
